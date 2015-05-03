b993d761d5c7b6e8c2cb927704602822e85abc69
zmelce <melce.husunbeyi@gmail.com>
2015-05-03 17:25:59 +0300

    Create JUnit Test Class for MathematicalOperations

---
+++ Assignment8/src/assignment8/test/TestMathematicalOperations.java
@@ -0,0 +1,19 @@
+package assignment8.test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import assignment8.MathematicalOperations;
+
+
+/**
+ * 
+ */
+
+/**
+ * @author melce
+ *
+ */
+public class TestMathematicalOperations {
+
+}
