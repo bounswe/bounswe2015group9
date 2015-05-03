847e2a5dbc88013abf630e0023575d9292676284
zmelce <melce.husunbeyi@gmail.com>
2015-05-03 18:07:46 +0300

    Create division function and its tests

--- Assignment8/src/assignment8/MathematicalOperations.java
+++ Assignment8/src/assignment8/MathematicalOperations.java
@@ -1,5 +1,19 @@
 package assignment8;
 
+/**
+ * provide several Mathematical Operations
+ */
 public class MathematicalOperations {
+	
+	
+	/**
+	 * divide two numbers
+	 * @param x is the dividend
+	 * @param y is the divisor
+	 * @return X divided by y
+	 */
+	public int division(int x, int y) {
 
+		return x / y;
+	}
 }
--- Assignment8/src/assignment8/test/TestMathematicalOperations.java
+++ Assignment8/src/assignment8/test/TestMathematicalOperations.java
@@ -1,19 +1,35 @@
 package assignment8.test;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 import assignment8.MathematicalOperations;
 
-
 /**
  * 
  */
 
 /**
- * @author melce
- *
+ *Test for MathematicalOperations Class
  */
 public class TestMathematicalOperations {
-
+	
+	/**
+	 * to test dividing by zero throw arithmetic exception
+	 */
+	@Test(expected = ArithmeticException.class)
+	public void testDivideByZero() {
+		MathematicalOperations tester = new MathematicalOperations();
+		tester.division(1000, 0);
+	}
+	
+	/**
+	 * to test general attitude of division
+	 */
+	@Test
+	public void testDivide() {
+		MathematicalOperations tester = new MathematicalOperations();
+		assertEquals("10 / 5 must be 2", 2, tester.division(10, 5));
+	}
 }
