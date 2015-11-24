cc2fcf8ce8a7a1061997d81e912bde7488e17f2e
umutuz7 <umutuz7@gmail.com>
2015-11-25 00:26:04 +0200

    Added NonSecure Controller for frontend tests

---
+++ Web-App/src/main/java/org/bounswe2015/group9/universal_access/controllers/UserNonSecureController.java
@@ -0,0 +1,42 @@
+package org.bounswe2015.group9.universal_access.controllers;
+
+import org.bounswe2015.group9.universal_access.dtos.UserDTO;
+import org.bounswe2015.group9.universal_access.entities.User;
+import org.bounswe2015.group9.universal_access.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by umut on 25.11.2015.
+ */
+@RestController
+@RequestMapping("/users")
+public class UserNonSecureController {
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO user) {
+        User createdUser = userService.createUser(user);
+        UserDTO createdUserDTO = new UserDTO(createdUser);
+        return new ResponseEntity<UserDTO>(createdUserDTO, HttpStatus.CREATED);
+    }
+    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> getUser(long id) {
+        User gotUser = userService.getUser(id);
+        UserDTO gotUserDTO = new UserDTO(gotUser);
+        return new ResponseEntity<UserDTO>(gotUserDTO, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{userId}", method = RequestMethod.PUT)
+    public ResponseEntity<UserDTO> updateUser(long id, @RequestBody UserDTO user) {
+        userService.updateUser(user);
+        return new ResponseEntity<UserDTO>(user, HttpStatus.ACCEPTED);
+    }
+}
