3e1620b8e02ee5fa1519f3cdab8d6aed251fa18b
omer temel <omertemel@omers-MacBook-Pro-3.local>
2015-11-30 22:19:43 +0200

    fixes

--- Web-App/src/main/java/org/bounswe2015/group9/universal_access/daos/impl/ViolationDao.java
+++
@@ -1,7 +0,0 @@
-package org.bounswe2015.group9.universal_access.daos.impl;
-
-import org.bounswe2015.group9.universal_access.entities.Violation;
-
-public class ViolationDao extends BaseDao<Violation, Long>{
-
-}
