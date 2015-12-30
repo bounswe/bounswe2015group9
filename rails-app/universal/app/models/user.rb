0b662e2c612ee1039a6fd55fc7a42b50656e0383
Omer M. Temel <omermtemel@gmail.com>
2015-12-30 20:50:52 +0200

    Add create credentials callback for User

--- rails-app/universal/app/models/user.rb
+++ rails-app/universal/app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :create_credentials!
+
+  protected
+    def create_credentials!
+      Doorkeeper::AccessToken.create!(
+        resource_owner_id: id,
+        scopes: Doorkeeper.configuration.scopes.to_s
+      )
+    end
 end
