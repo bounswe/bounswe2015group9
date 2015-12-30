ef3c545fd004e569e2786b533d128f8c2d34f34c
Omer M. Temel <omermtemel@gmail.com>
2015-12-30 21:22:21 +0200

    Add tag model

--- rails-app/universal/db/schema.rb
+++ rails-app/universal/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230185523) do
+ActiveRecord::Schema.define(version: 20151230192158) do
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "user_id",                    null: false
@@ -52,6 +52,26 @@ ActiveRecord::Schema.define(version: 20151230185523) do
   end
 
   add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type", limit: 255
+    t.integer  "tagger_id"
+    t.string   "tagger_type",   limit: 255
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",           limit: 255
+    t.integer "taggings_count",             default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
