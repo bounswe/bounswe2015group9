48a3182473d1f0695ec1e6ddca827d9efaa55eb7
Omer M. Temel <omermtemel@gmail.com>
2015-12-31 04:00:02 +0200

    Complete authentication

---
+++ rails-app/universal/app/controllers/registrations_controller.rb
@@ -0,0 +1,53 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_sign_up_params, only: [:create]
+  before_filter :configure_account_update_params, only: [:update]
+
+
+  def new
+    super
+  end
+
+
+  def create
+    super
+  end
+
+
+  def edit
+    super
+  end
+
+
+  def update
+    super
+  end
+
+
+  def destroy
+    super
+  end
+
+  def cancel
+    super
+  end
+
+  protected
+
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :gender, :age]
+  end
+
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :gender, :age]
+  end
+
+  def after_sign_up_path_for(resource)
+    super(resource)
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
+end
--- rails-app/universal/config/routes.rb
+++ rails-app/universal/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   root 'home#index'
 
