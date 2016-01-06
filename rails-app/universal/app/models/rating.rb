ac1dc042a6adfe166ecc56fa6148671261f3dbab
Omer M. Temel <omermtemel@gmail.com>
2016-01-06 23:55:18 +0200

    Add rating to views

--- rails-app/universal/app/models/rating.rb
+++ rails-app/universal/app/models/rating.rb
@@ -2,6 +2,17 @@ class Rating < ActiveRecord::Base
   belongs_to :violation
   belongs_to :user
 
+  before_validation :check_duplication
+
   validates :score, inclusion: { in: [1, -1],
     message: "%{value} is not a valid score" }
+
+  private
+    def check_duplication
+      if Rating.where(violation: self.violation, user: self.user).present?
+        false
+      else
+        true
+      end
+    end
 end
