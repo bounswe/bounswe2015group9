1911190e65605dfb7913007642670327c1085435
Omer M. Temel <omermtemel@gmail.com>
2016-01-07 15:59:15 +0200

    Vilation Serilization Bug Solved

--- rails-app/universal/app/serializers/violation_detail_serializer.rb
+++ rails-app/universal/app/serializers/violation_detail_serializer.rb
@@ -1,6 +1,15 @@
 class ViolationDetailSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :closed, :created_at, :address, :tag_list
+  attributes :id, :title, :description, :image_url, :closed, :created_at, :address, :tag_list
 
+  def attributes
+  	data = super
+  	if object.ratings.nil?
+  		data[:rating] = object.ratings.sum(:score)
+  	else
+  		data[:rating] = 0
+  	end
+  	data
+  end
   has_one :user
   has_one :type
   has_one :city
--- rails-app/universal/app/serializers/violation_serializer.rb
+++ rails-app/universal/app/serializers/violation_serializer.rb
@@ -1,6 +1,16 @@
 class ViolationSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :closed, :created_at, :tag_list
+  attributes :id, :title, :description, :closed, :image_url, :created_at, :tag_list
 
+  def attributes
+  	data = super
+  	if object.ratings.nil?
+  		data[:rating] = object.ratings.sum(:score)
+  	else
+  		data[:rating] = 0
+  	end
+  	data
+  end
+  
   has_one :user
   has_one :type
   has_one :city
